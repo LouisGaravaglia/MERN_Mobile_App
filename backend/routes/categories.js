a4ac5cff76082c2b66d0fe31b79ae27f8f423a31
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-02-24 15:32:43 -0800

    finished categories route with put and delete and get by id routes

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -5,9 +5,46 @@ const Category = require("../models/category")
 router.get(`/`, async (req, res) => {
     try {
         const categoryList = await Category.find()
-        res.send(categoryList);
+        res.status(200).send(categoryList);
     } catch (e) {
         res.status(500).json({success: false})
+    };
+});
+
+router.get(`/:id`, async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        if (category) {
+            res.status(200).send(category);
+        } else {
+            res.status(500).json({success:false, message: "the category can not be found"})
+        }
+
+    } catch (e) {
+        res.status(500).json({success: false, message: e})
+    };
+});
+
+router.put(`/:id`, async (req, res) => {
+    try {
+        const category = await Category.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name,
+                icon: req.body.icon,
+                color: req.body.color
+            },
+            { new: true}
+        );
+
+        if (category) {
+            res.status(200).send(category);
+        } else {
+            res.status(500).json({success:false, message: "the category can not be found"})
+        }
+
+    } catch (e) {
+        res.status(500).json({success: false, message: e})
     };
 });
 
@@ -26,5 +63,19 @@ router.post("/", async (req, res) => {
     };
 });
 
+router.delete("/:id", async (req, res) => {
+
+    try {
+        const deletedCategory = await Category.findByIdAndRemove(req.params.id)
+        if (deletedCategory) {
+            return res.status(200).json({success:true, message: "the category is deleted"})
+        } else {
+            return res.status(404).json({success:true, message: "category not found"})
+        };
+    } catch (e) {
+        return res.status(400).json({success:false, message: e})
+    };
+
+})
 
 module.exports = router;
