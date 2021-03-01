f1521c6d859eee8342d4d80c86366a9fbc71c3ae
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-03-01 12:08:05 -0800

    placing an order in the orders route is working

--- backend/models/order.js
+++ backend/models/order.js
@@ -2,12 +2,58 @@ const mongoose = require("mongoose");
 
 //GOOGLE MONGOOSE SCHEMA TO SEE DOCS REGARDING SCHEMA CONSTRUCTION
 const orderSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required:true
+    }],
+    shippingAddress1: {
+        type: String,
+        required: true,
+    },
+    shippingAddress2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending',
+    },
+    totalPrice: {
         type: Number,
-        required: true
-    }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true,
 });
 
 const Order = mongoose.model("Order", orderSchema);
